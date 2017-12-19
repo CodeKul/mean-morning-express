c68ed0281d06780c64845beab674e29748f342d3
aniruddha <aniruddha.kudalkar@gmail.com>
2017-12-19 10:09:29 +0530

    post : body parsers

--- first/app.js
+++ first/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get(
-    '/', 
+    '/',
     (req, res) => res.send('Hello World!')
 )
 
 app.get(
-    '/calcv1/:num1/:num2', 
+    '/calcv1/:num1/:num2',
     (req, res) => {
         console.log(req.params)
         res.send(`Addition of ${req.params['num1']} and ${req.params['num2']} is ${parseInt(req.params['num1']) + parseInt(req.params['num2'])} `)
@@ -18,13 +23,27 @@ app.get(
 app.post(
     '/calcv2',
     (req, res) => {
-        console.log(req.body)
-        res.end()
+
+        let custRes = {}
+        if (req.body.operation === '+') {
+            custRes = {
+                sts: 200,
+                msg: 'addition',
+                res: (req.body.num1 + req.body.num2)
+            }
+        }
+        else {
+            custRes = {
+                sts: 400,
+                msg: 'error',
+                res: 0
+            }
+        }
+        res.json(custRes)
     }
 )
 
 app.listen(
-    3000, 
+    3000,
     () => console.log('Example app listening on port 3000!')
 )
-

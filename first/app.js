7a29d97148a5e31a14da5d438c84f42eebe58c40
aniruddha <aniruddha.kudalkar@gmail.com>
2017-12-20 10:04:57 +0530

    express: post details

--- first/app.js
+++ first/app.js
@@ -2,10 +2,31 @@ const express = require('express')
 const bodyParser = require('body-parser');
 
 const app = express()
+let logs = Array()
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get(
     '/',
@@ -23,22 +44,27 @@ app.get(
 app.post(
     '/calcv2',
     (req, res) => {
-
+        //console.log(req.body)
         let custRes = {}
         if (req.body.operation === '+') {
             custRes = {
                 sts: 200,
                 msg: 'addition',
-                res: (req.body.num1 + req.body.num2)
+                res: (parseInt(req.body.num1) + parseInt(req.body.num2))
             }
-        }
-        else {
+        } else {
             custRes = {
                 sts: 400,
                 msg: 'error',
                 res: 0
             }
         }
+
+        let logged = {
+            [Date.now()]: custRes
+        }
+        logs.push(logged)
+        console.log(logged)
         res.json(custRes)
     }
 )

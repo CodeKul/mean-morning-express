08368b5aa434b7dc16b1a7764f06e60660335657
aniruddha <aniruddha.kudalkar@gmail.com>
2018-01-18 09:42:50 +0530

    mongoose

--- first/my.js
+++ first/my.js
@@ -1,6 +1,37 @@
-let num1  = 10;
-let num2 = 30;
-let op = '+'
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-console.log(eval(`${num1} ${op} ${num2}`))
+const empSchm = mongoose.Schema({
+    id: Number,
+    name: String,
+    sal: Number
+});
+
+const Employee = mongoose.model('Employee', empSchm);
+
+let manager = new Employee({
+    id: 7896,
+    name: 'Android',
+    sal: 799
+})
+
+manager.save().then(() => {
+    console.log(`Manager Has been Saved`)
+})
+
+Employee.find({ }, (err, mgrs) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    mgrs.forEach(mgr => {
+        console.log(mgr.name)
+    });
+});
+
+
+
+
+
+
 
